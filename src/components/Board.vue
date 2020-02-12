d772003d0b178b1a89f65cbf96ca06aaad6b56a1
Your Actual Name <Your Actual Email>
2020-02-12 12:16:28 +0900

    ADD - スペースを4から2へ変更し全体で統一

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,21 +1,21 @@
 <template>
-    <div>
-        <header>
-            My Trello
-        </header>
-        <main>
-            <p class="info-line">All: 0 tasks</p>
-            <list-add></list-add>
-        </main>
-    </div>
+  <div>
+    <header>
+      My Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <list-add></list-add>
+    </main>
+  </div>
 </template>
 
 <script>
 import ListAdd from './ListAdd.vue'
 
 export default {
-    components: {
-        ListAdd
-    }
+  components: {
+    ListAdd
+  }
 }
 </script>
