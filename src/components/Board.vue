8f866e29ee3bab56c2b8427024e5400961a83bd5
takummy <40820363+takummy@users.noreply.github.com>
2020-02-12 19:00:37 +0900

    Feature/create list#2 (#6)
    * NEW - Vue CLIでプロジェクト作成
    
    * NEW - Board.vueを作成し、コンポーネントへ登録
    
    * NEW - TOPページにCSSを適用
    
    * NEW - ListAddコンポーネントを作成
    
    * Add - AddListのactionを定義
    
    * Add - AddListのmutationを定義
    
    * Add - AddListのstateを定義
    
    * Add - localStorageへデータを保存できるよう実装
    
    * ADD - フォームへ動的にスタイルを適用できるよう修正
    
    * ADD - スペースを4から2へ変更し全体で統一
    
    * NEW - Listコンポーネントを作成
    
    * ADD - Board.vueでmapStateを使用し、リストを表示するよう追記
    
    * ADD - List削除機能を作成

---
+++ src/components/Board.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header>
+      My Trello
+    </header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+              :key="item.id"
+              :title="item.title"
+              :listIndex="index"
+        />
+        <list-add />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import ListAdd from './ListAdd'
+import List from './List'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    ListAdd,
+    List
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ])
+  }
+}
+</script>
