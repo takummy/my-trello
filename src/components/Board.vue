259f92b25b06abf4ccd7d4a28448d8ccc7b00649
Your Actual Name <Your Actual Email>
2020-02-13 17:22:33 +0900

    ADD - Board内でListをドラッグ&ドロップできるよう追記

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,9 @@
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists"
+                 class="list-index"
+                 @end="movingList">
         <list v-for="(item, index) in lists"
               :key="item.id"
               :title="item.title"
@@ -14,12 +16,13 @@
               @change="movingCard"
         />
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
 import ListAdd from './ListAdd'
 import List from './List'
 import { mapState } from 'vuex'
@@ -27,7 +30,8 @@ import { mapState } from 'vuex'
 export default {
   components: {
     ListAdd,
-    List
+    List,
+    draggable
   },
   computed: {
     ...mapState([
@@ -39,6 +43,9 @@ export default {
   },
   methods: {
     movingCard: function() {
+      this.$store.dispatch('updateList', { lists: this.lists })
+    },
+    movingList: function() {
       this.$store.dispatch('updateList', { lists: this.lists })
     }
   }
