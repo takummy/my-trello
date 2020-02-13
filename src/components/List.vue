57b2ce1c8142d7cf77a83bc6fc385f2075ceea17
Your Actual Name <Your Actual Email>
2020-02-13 17:05:03 +0900

    ADD - List間でもCardをドラッグ＆ドロップできるよう修正

--- src/components/List.vue
+++ src/components/List.vue
@@ -5,7 +5,9 @@
       <p class="list-counter">total: {{ totalCardInList }}</p>
       <div class="deletelist" @click="removeList">×</div>
     </div>
-    <draggable>
+    <draggable group="cards"
+               :list="cards"
+               @end="$emit('change')">
       <card v-for="(item, index) in cards"
             :body="item.body"
             :key="item.id"
